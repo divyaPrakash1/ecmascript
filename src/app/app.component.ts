095f8afa935822f8443d6002e78ffd7b1848ec86
Divya Prakash Mishra <iamdpm93@gmail.com>
2025-01-10 12:31:15 +0530

    extra

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    console.log('Dadda')
+  }
+  items: Array<{title: string, link: string}> = [
+    { title: 'Explore the Docs', link: 'https://angular.dev' },
+    { title: 'Learn with Tutorials', link: 'https://angular.dev/tutorials' },
+    { title: 'CLI Docs', link: 'https://angular.dev/tools/cli' },
+    { title: 'Angular Language Service', link: 'https://angular.dev/tools/language-service' },
+    { title: 'Angular DevTools', link: 'https://angular.dev/tools/devtools' },
+  ];
   title = 'ES_Practice';
 }
