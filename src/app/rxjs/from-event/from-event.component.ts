33872ee43119ef5e30c8c41b9efb6de0a1ee2f2c
Divya Prakash Mishra <iamdpm93@gmail.com>
2025-07-22 15:22:20 +0530

    from event

--- src/app/rxjs/from-event/from-event.component.ts
+++ src/app/rxjs/from-event/from-event.component.ts
@@ -12,11 +12,17 @@ export class FromEventComponent implements OnInit, AfterViewInit, OnDestroy {
   constructor(private _common: CommonService) { }
   @ViewChild('addBtn') addBtn!: ElementRef;
 
+  @ViewChild('clickCount') clickCount!: ElementRef;
+
+
+  @ViewChild('addMoreCount') addMoreCount!: ElementRef;
+  @ViewChild('annny') annny!: ElementRef;
+
   ngOnInit(): void {
 
   }
 
-  ngAfterViewInit(): void {
+  example1() {
     let count = 1;
     this.subscription = fromEvent(this.addBtn.nativeElement, 'click').subscribe(res => {
       // console.log('Video : ', count);
@@ -26,9 +32,56 @@ export class FromEventComponent implements OnInit, AfterViewInit, OnDestroy {
     })
   }
 
-  print() {
-    // elContainer
+  ngAfterViewInit(): void {
+    this.example1();
+    this.example2();
+    this.example3();
+    this.example4();
   }
+
+  example2() {
+    let count = 1;
+    this.subscription = fromEvent<any>(this.clickCount.nativeElement, 'click').subscribe(res => {
+      this.print(count, 'clickCount');
+      count++;
+    })
+  }
+
+  print(value: any, id: any) {
+    if (typeof document !== 'undefined') {
+      let el = document.createElement('li');
+      el.innerText = value;
+      document.getElementById(id)?.appendChild(el);
+    }
+  }
+
+
+
+  example3() {
+    let ct = 1;
+    this.subscription = fromEvent<any>(this.addMoreCount.nativeElement, 'click').subscribe(() => {
+      this.print1(ct, 'addMoreCount');
+      ct++;
+    })
+  }
+  example4() {
+    let ct = 1;
+    this.subscription = fromEvent<any>(this.annny.nativeElement, 'change').subscribe(() => {
+      this.print1(ct, 'annsny');
+      ct++;
+    })
+  }
+
+  print1(value: number, id: string) {
+    if (typeof document !== undefined) {
+      let el = document.createElement('li');
+      el.innerText = value + "";
+      document.getElementById(id)?.appendChild(el);
+    }
+
+  }
+
+
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
