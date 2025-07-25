c2f7ff137d54cfa6a026f082390edf58a826def7
Divya Prakash Mishra <iamdpm93@gmail.com>
2025-07-25 17:01:33 +0530

    switchMap revise with search functionality

--- src/app/rxjs/switch-map2/switch-map2.component.ts
+++ src/app/rxjs/switch-map2/switch-map2.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { concatMap, debounceTime, distinctUntilChanged, filter, map, mergeMap, pluck, Subject, switchMap, takeUntil } from 'rxjs';
+import { concatMap, debounceTime, distinctUntilChanged, filter, map, mergeMap, pluck, Subject, Subscription, switchMap, takeUntil } from 'rxjs';
 import { CommonService } from '../service/comman.service';
 import { Product } from '../interface/product.interface';
 
@@ -12,39 +12,76 @@ import { Product } from '../interface/product.interface';
 export class SwitchMap2Component implements AfterViewInit, OnDestroy {
 
   @ViewChild('searchForm') searchForm!: NgForm
+  @ViewChild('searchForm2') searchForm2!: NgForm
   private destroy$ = new Subject<void>();
   data: Array<Product> = [];
+
+  subscription1!: Subscription;
+  subscription2!: Subscription;
   constructor(private _common: CommonService) { }
   ngAfterViewInit(): void {
     // this._common.getFilteredProducts("Gym Towel").subscribe(res => {
     //   console.log(res);
     // })
 
-    const formValues = this.searchForm.valueChanges;
-    formValues && formValues
-      .pipe(
-        // map(data => data['serachTerm'])
-        filter(() => !!this.searchForm.valid),
-        pluck('serachTerm'),
-        debounceTime(1000),
-        distinctUntilChanged(),
-        takeUntil(this.destroy$),
-        // map(res => {
-        //   st = res
-        // }),
-        switchMap(res => this._common.getFilteredProducts(res))
-      )
-      .subscribe(res => {
-        // res = res.filter(r => r.title.includes(st))
-        // console.log(res);
-        this.data = res;
-      })
+    // this.example1();
+    this.example2();
+
   }
 
+  example1() {
+    const formValues = this.searchForm.valueChanges;
+    if (formValues) {
+
+      this.subscription1 = formValues
+        .pipe(
+          // map(data => data['serachTerm'])
+          filter(() => !!this.searchForm.valid),
+          pluck('serachTerm'),
+          debounceTime(1000),
+          distinctUntilChanged(),
+          takeUntil(this.destroy$),
+          // map(res => {
+          //   st = res
+          // }),
+          switchMap(res => this._common.getFilteredProducts(res))
+        )
+        .subscribe(res => {
+          // res = res.filter(r => r.title.includes(st))
+          // console.log(res);
+          this.data = res;
+        })
+    }
+  }
+
+  example2() {
+    const formValue = this.searchForm2.valueChanges;
+
+    if (formValue) {
+
+      this.subscription2 = formValue
+        .pipe(
+          filter(() => !!this.searchForm2.valid),
+          debounceTime(500),
+          map(e => e.serachTerm2),
+          takeUntil(this.destroy$),
+          distinctUntilChanged(),
+          switchMap(e => this._common.getFilteredProducts(e))
+        ).subscribe(res => {
+          console.log(res);
+        })
+    }
+  }
 
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
+    if (this.subscription1) {
+      this.subscription1.unsubscribe();
+    }
+    if (this.subscription2) {
+      this.subscription2.unsubscribe();
+    }
   }
 }
 
