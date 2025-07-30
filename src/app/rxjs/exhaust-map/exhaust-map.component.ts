b9c712b15631a9863c3b7a69b25c469d68c49105
Divya Prakash Mishra <iamdpm93@gmail.com>
2025-07-30 12:10:01 +0530

    exhaustMap revise

--- src/app/rxjs/exhaust-map/exhaust-map.component.ts
+++ src/app/rxjs/exhaust-map/exhaust-map.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, OnDestroy, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonService } from '../service/comman.service';
 import { Product1 } from '../interface/product.interface';
-import { concatMap, exhaustMap, fromEvent, Observable, tap } from 'rxjs';
+import { exhaustMap, fromEvent, interval, Observable, Subscription, take, tap } from 'rxjs';
 
 @Component({
   selector: 'ecmspt-exhaust-map',
@@ -14,41 +14,59 @@ import { concatMap, exhaustMap, fromEvent, Observable, tap } from 'rxjs';
 export class ExhaustMapComponent implements OnInit, AfterViewInit, OnDestroy {
   fetching: boolean = false;
   num: number = 0;
+  num1: number = 0;
   products: Array<Product1> = [];
 
   @ViewChild('btn') btn!: ElementRef;
+  @ViewChild('btn1') btn1!: ElementRef;
+
+  subscription1!: Subscription;
+  subscription2!: Subscription;
 
   constructor(private _common: CommonService) {
 
   }
 
   ngOnInit(): void {
-    this._common.exhaustMapSubject.pipe(
-
-    ).subscribe(res => {
-      console.log(res);
-      this.products = res;
-    });
+    this._common.exhaustMapSubject
+      .pipe()
+      .subscribe(res => {
+        this.products = res;
+      });
   }
 
   ngAfterViewInit(): void {
-    fromEvent(this.btn.nativeElement, 'click')
+    // this.example1();
+    this.example2();
+  }
+
+  example1() {
+    this.subscription1 = fromEvent(this.btn.nativeElement, 'click')
       .pipe(
         tap(() => this.fetching = true),
         exhaustMap(async () => this.onSave())
       ).subscribe((res) => {
-        console.log("object", res);
         // this.products = res
         this.fetching = false;
-      }
-
-      )
+      })
   }
 
-
-  btnClick() {
-    this.num++;
+  example2() {
+    this.subscription2 = fromEvent(this.btn1.nativeElement, 'click').pipe(
+      exhaustMap(() => this.getPosts())
+    ).subscribe(res => {
+      console.log('exhaustMap', res);
+    })
   }
+
+  getPosts(): Observable<any> {
+    this.num1++;
+    return this._common.getPosts(this.num1)
+  }
+
+  // btnClick() {
+  //   this.num++;
+  // }
 
   onSave() {
     this.num++
@@ -60,7 +78,12 @@ export class ExhaustMapComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-
+    if (this.subscription1) {
+      this.subscription1.unsubscribe();
+    }
+    if (this.subscription2) {
+      this.subscription2.unsubscribe();
+    }
   }
 }
-// this string is test to test number of test available in this string to test        
+// this string is test to test number of test available in this string to test
